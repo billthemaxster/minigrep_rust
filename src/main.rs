27ae3fe5b9dbfadf81d1bf0f613c3fc251792f43
Martin Kennish <billthemaxster@gmail.com>
2020-08-09 22:50:44 +0100

    Use a constructor for config instead of a method

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.filename);
@@ -17,12 +17,14 @@ fn main() {
 
 struct Config {
     query: String,
-    filename: String
+    filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query : String = args[1].clone();
-    let filename : String = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
 
-    Config {query, filename}
+        Config { query, filename }
+    }
 }
