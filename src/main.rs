ff97fcf9e7ef30824413642f2f51ace0c030fd94
Martin Kennish <billthemaxster@gmail.com>
2020-08-09 23:08:12 +0100

    Extract function for running program

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,15 @@ fn main() {
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the contents of the file");
 
     println!("With text:\n{}", contents);
+
 }
 
 struct Config {
