2508856272abdd41c9a77598c66797eedb048961
Martin Kennish <billthemaxster@gmail.com>
2020-08-23 22:41:57 +0100

    Implement case insenstive search using environment variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,15 +16,22 @@ impl Config {
 
         let query: String = args[1].clone();
         let filename: String = args[2].clone();
+        let case_sensitive: bool = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -39,12 +48,23 @@ pub fn search<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query){
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result(){
+    fn case_sensitive_one_result(){
         let query = "duct";
         let contents = "\
 Rust:
@@ -55,7 +75,7 @@ Pick three.";
     }
 
     #[test]
-    fn two_results() {
+    fn case_sensistive_two_results() {
         let query = "duct";
         let contents = "\
 There are many ducts in this building,
@@ -63,5 +83,20 @@ Some of them are productive
 Others are not";
 
         assert_eq!(vec!["There are many ducts in this building,","Some of them are productive"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive_two_result() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:","Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
