9c3b0e717277f8d04bd89ae9afcd1ed0713dbed9
Martin Kennish <billthemaxster@gmail.com>
2020-08-23 22:45:10 +0100

    print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
 
     let config = Config::new(&args)
         .unwrap_or_else(|err| {
-            println!("Problem parsing arguments: {}", err);
+            eprintln!("Problem parsing arguments: {}", err);
             process::exit(1);
         });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application err {}", e);
+        eprintln!("Application err {}", e);
         process::exit(1);
     }
 }
